071df55b9252a99f7265178bf76cfbebef739650
Anya <you@example.com>
2025-11-12 23:40:46 +0700

    Прописаны страницы и роутеры аутентификации

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloРage from './pages/HelloPage';
+import HelloPage from './pages/HelloPage';
+import RegistrationPage from './pages/RegistrationPage';
+import CodePage from './pages/CodePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <HelloРage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HelloPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/check_code" element={<CodePage />} />
+      </Routes>
+    </Router>
   );
 }
 
